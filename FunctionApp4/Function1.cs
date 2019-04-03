e600ee523cf9af0645ac115782b6c4fa8a5400a2
Martin Slot <slot@sigsys.dk>
2019-04-03 22:56:43 +0200

    Removed unused usings

--- FunctionApp4/Function1.cs
+++ FunctionApp4/Function1.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace FunctionApp4
